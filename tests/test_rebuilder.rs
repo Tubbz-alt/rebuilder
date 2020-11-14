4b0ffee8be31a7e09930af62afa2d8846339ff48
Jelle van der Waa <jelle@vdwaa.nl>
2020-11-15 00:16:49 +0100

    Figured out .. tempfile

--- tests/test_rebuilder.rs
+++ tests/test_rebuilder.rs
@@ -1,12 +1,58 @@
 use rstest::{fixture, rstest};
 use std::fs;
-use std::path::PathBuf;
-use std::path::Path;
-use tempfile::tempdir;
+use tempfile::TempDir;
+use tempfile::Builder;
 use std::fs::File;
-use std::io::{self, Write};
+use std::io::{Write};
+//use flate2::Compression;
+//use flate2::write::GzEncoder;
+use std::collections::HashMap;
+use tar::Builder as TarBuilder;
 
 
+fn get_pkg_desc(pkgname: String, depends: Vec<String>, makedepends: Vec<String>) -> String {
+    let fields: HashMap<&str, &str> = [("FILENAME", "foo.tar.zst"),
+                                       ("NAME", &pkgname),
+                                       ("BASE", &pkgname), 
+                                       ("VERSION", "1.0-1"),
+                                       ("DESC", "pkg"),
+                                       ("ARCH", "x86_64"),
+                                       ("BUILDDATE", "1590150671"),
+                                       ("PACKAGER", "Crab"),
+                                       ("URL", "https://archlinux.org"),
+                                       ("LICENSES", "MIT"),
+        ].iter().cloned().collect();
+
+    let mut desc = String::from("");
+
+    // Common fields
+    for (key, val) in fields.iter() {
+        let key = format!("%{}%\n", key.to_owned());
+        desc.push_str(&key);
+        let val = format!("{}\n", val);
+        desc.push_str(&val);
+    }
+
+    if !depends.is_empty() {
+        desc.push_str("%DEPENDS%\n");
+        for dep in depends.iter() {
+            desc.push_str(dep);
+        }
+        desc.push_str("\n");
+    }
+
+    if !makedepends.is_empty() {
+        desc.push_str("%MAKEDEPENDS%\n");
+        for dep in makedepends.iter() {
+            desc.push_str(dep);
+        }
+        desc.push_str("\n");
+    }
+
+    dbg!(desc.clone());
+
+    desc
+}
 
 #[fixture]
 pub fn invalid_data() -> (Vec<String>, Option<String>) {
@@ -26,47 +72,69 @@ fn should_panic(invalid_data: (Vec<String>, Option<String>)) {
 
 
 #[fixture]
-fn no_reverse_deps() -> (Vec<String>, Option<String>) {
+fn no_reverse_deps() -> (Vec<String>, Option<String>, TempDir) {
+    let rootdir = Builder::new().prefix("example").tempdir().unwrap();
+    let dbpath = rootdir.path().display().to_string();
     let pkgname = String::from("testpkg1");
+    let repos = vec!["core", "extra", "community", "multilib"];
     let pkgnames = vec![pkgname];
-    let rootdir = Path::new("/tmp/.rebuilderd");
 
-    fs::create_dir(rootdir).unwrap();
+    // /var/lib/pacman/local
+    let localdir = format!("{}/local", dbpath);
+    fs::create_dir(localdir).unwrap();
+    let file_path = format!("{}/local/ALPM_DB_VERSION", dbpath);
+    let mut file = File::create(file_path).unwrap();
+    // TODO: define const
+    writeln!(file, "9").unwrap();
 
-    let localdir = rootdir.join("local/");
-    fs::create_dir_all(localdir).unwrap();
+    // /var/lib/pacman/sync
+    let syncdir = format!("{}/sync", dbpath);
+    fs::create_dir(syncdir).unwrap();
 
-    // Create core,extra,community,multilib db's
-    let syncdir = rootdir.join("sync");
+    // every db needs at least one pkg to be valid.
+    for repo in &repos {
+        dbg!("loop 1");
+        let name = format!("{}.db", repo);
+        let repodir = Builder::new().prefix(&name).tempdir().unwrap();
+        let repodirstr = repodir.path().display().to_string();
+        let placeholder = String::from("placeholder");
+        let placeholderstr = placeholder.clone();
+        let desc = get_pkg_desc(placeholder, vec![], vec![]);
+
+        fs::create_dir(format!("{}/{}", repodirstr, placeholderstr)).unwrap();
+        let file_path = format!("{}/{}/desc", repodirstr, placeholderstr);
+        let desc_path = file_path.clone();
+        let mut file = File::create(file_path).unwrap();
+        writeln!(file, "{}", desc).unwrap();
+
+        let db = File::create("repo.tar").unwrap();
+        let mut tar = TarBuilder::new(file);
+        dbg!("woop");
+        //(desc_path, format!("{}-1.0-1/desc", placeholderstr)).unwrap();
+        //tar.append_path_with_name(desc_path, format!("{}-1.0-1/desc", placeholderstr)).unwrap();
+        dbg!("ninin");
+    }
+    /*
     for pkg in &pkgnames {
         let path = syncdir.clone().join(format!("{}-1-1", pkg));
         dbg!(path.as_path().display());
         fs::create_dir_all(path).unwrap();
     }
+    */
 
-    let file_path = syncdir.join("ALPM_DB_VERSION");
-    let mut file = File::create(file_path).unwrap();
-    // TODO: define const
-    writeln!(file, "9").unwrap();
-
-    let dbpath = Some(rootdir.display().to_string());
-
-    (pkgnames, dbpath)
+    (pkgnames, Some(dbpath), rootdir)
 }
 
-
 #[rstest]
-fn test_reverse_deps(no_reverse_deps: (Vec<String>, Option<String>)) {
+fn test_reverse_deps(no_reverse_deps: (Vec<String>, Option<String>, TempDir)) {
+    println!("lala");
     let pkgnames = no_reverse_deps.0;
     let dbpath = no_reverse_deps.1;
     let dbpath2 = dbpath.clone();
-
-    /*
+    println!("googgo");
     for entry in fs::read_dir(dbpath.unwrap()).unwrap() {
         dbg!(entry);
     }
-    */
-    rebuilder::run(pkgnames, dbpath, None).unwrap();
 
-    fs::remove_dir_all(dbpath2.unwrap());
+    //rebuilder::run(pkgnames, dbpath, None).unwrap();
 }
