879f5e846e1dbc78323f4ae561055fc6908709ee
Jelle van der Waa <jelle@vdwaa.nl>
2020-11-15 22:43:04 +0100

    Add struct Package class for tests
    Make a Struct Package to hold test packages for testing and re-usable
    functions for creating the repository databases.

--- tests/test_rebuilder.rs
+++ tests/test_rebuilder.rs
@@ -1,65 +1,90 @@
 use rstest::{fixture, rstest};
+use std::convert::TryFrom;
 use std::fs;
-use tempfile::TempDir;
-use tempfile::Builder;
 use std::fs::File;
-use std::io::{Write};
-use std::collections::HashMap;
+use std::io::Write;
 use tar::Builder as TarBuilder;
 use tar::Header;
-use std::convert::TryFrom;
-
+use tempfile::Builder;
+use tempfile::TempDir;
 
 const ALPM_DB_VERSION: &str = "9";
 
+#[derive(Hash, Eq, PartialEq, Debug)]
+struct Package {
+    name: String,
+    base: String,
+    version: String,
+    depends: Vec<String>,
+    makedepends: Vec<String>,
+}
 
-fn get_pkg_desc(pkgname: String, depends: Vec<String>, makedepends: Vec<String>) -> String {
-    let fields: HashMap<&str, &str> = [("FILENAME", "foo.tar.zst"),
-                                       ("NAME", &pkgname),
-                                       ("BASE", &pkgname), 
-                                       ("VERSION", "1.0-1"),
-                                       ("DESC", "pkg"),
-                                       ("ARCH", "x86_64"),
-                                       ("BUILDDATE", "1590150671"),
-                                       ("PACKAGER", "Crab"),
-                                       ("URL", "https://archlinux.org"),
-                                       ("LICENSES", "MIT"),
-        ].iter().cloned().collect();
-
-    let mut desc = String::from("");
-
-    // Common fields
-    for (key, val) in fields.iter() {
-        let key = format!("%{}%\n", key.to_owned());
-        desc.push_str(&key);
-        let val = format!("{}\n", val);
-        desc.push_str(&val);
-        desc.push_str("\n");
-    }
-
-    if !depends.is_empty() {
-        desc.push_str("%DEPENDS%\n");
-        for dep in depends.iter() {
-            desc.push_str(dep);
+impl Package {
+    fn new(
+        name: &str,
+        base: &str,
+        version: &str,
+        depends: Vec<String>,
+        makedepends: Vec<String>,
+    ) -> Package {
+        Package {
+            name: name.to_string(),
+            base: base.to_string(),
+            version: version.to_string(),
+            depends: depends,
+            makedepends: makedepends,
         }
-        desc.push_str("\n");
     }
 
-    if !makedepends.is_empty() {
-        desc.push_str("%MAKEDEPENDS%\n");
-        for dep in makedepends.iter() {
-            desc.push_str(dep);
+    fn desc(&self) -> String {
+        let mut desc = String::from("");
+
+        let name = format!("%NAME%\n{}\n", self.name);
+        desc.push_str(&name);
+
+        let base = format!("%BASE%\n{}\n", self.base);
+        desc.push_str(&base);
+
+        if !self.depends.is_empty() {
+            desc.push_str("%DEPENDS%\n");
+            for dep in self.depends.iter() {
+                desc.push_str(dep);
+            }
+            desc.push_str("\n");
         }
-        desc.push_str("\n");
+
+        if !self.makedepends.is_empty() {
+            desc.push_str("%MAKEDEPENDS%\n");
+            for dep in self.makedepends.iter() {
+                desc.push_str(dep);
+            }
+            desc.push_str("\n");
+        }
+
+        desc
     }
 
-    desc
+    fn path(&self) -> String {
+        format!("{}-{}/desc", self.name, self.version)
+    }
+
+    fn tarheader(&self) -> Header {
+        let mut header = Header::new_gnu();
+        let desc = self.desc();
+        let datalen = u64::try_from(desc.len()).unwrap();
+        header.set_path(self.path()).unwrap();
+        header.set_size(datalen);
+        header.set_mode(0o644);
+        header.set_cksum();
+
+        header
+    }
 }
 
 #[fixture]
 pub fn invalid_data() -> (Vec<String>, Option<String>) {
     let pkgnames = vec![String::from("testpkg1")];
-    let dbpath = Some(String::from("/non-existant-path")); 
+    let dbpath = Some(String::from("/non-existant-path"));
 
     (pkgnames, dbpath)
 }
@@ -72,54 +97,103 @@ fn should_panic(invalid_data: (Vec<String>, Option<String>)) {
     rebuilder::run(pkgnames, dbpath, vec![], None).unwrap();
 }
 
-
-#[fixture]
-fn no_reverse_deps() -> (Vec<String>, Option<String>, Vec<String>, TempDir) {
-    let rootdir = Builder::new().prefix("example").tempdir().unwrap();
+fn init_repo() -> (TempDir, String, String) {
+    // TODO: do we need a root dir
+    let rootdir = Builder::new().prefix("no_reverse_deps").tempdir().unwrap();
     let dbpath = rootdir.path().display().to_string();
-    let pkgname = String::from("testpkg1");
-    let reponame = String::from("test");
-    let repos = vec![reponame];
-    let pkgnames = vec![pkgname.clone()];
-
-    // /var/lib/pacman/local
+    // local dir
     let localdir = format!("{}/local", dbpath);
     fs::create_dir(localdir).unwrap();
     let file_path = format!("{}/local/ALPM_DB_VERSION", dbpath);
     let mut file = File::create(file_path).unwrap();
     writeln!(file, "{}", ALPM_DB_VERSION).unwrap();
 
-    // /var/lib/pacman/sync
+    // sync dir
     let syncdir = format!("{}/sync", dbpath);
     fs::create_dir(&syncdir).unwrap();
 
-    let desc = get_pkg_desc(pkgname, vec![], vec![]);
-    let mut header = Header::new_gnu();
+    (rootdir, dbpath, syncdir)
+}
 
+#[fixture]
+fn no_reverse_deps() -> (Vec<String>, Option<String>, Vec<String>, TempDir) {
+    let reponame = String::from("test");
+    let (rootdir, dbpath, syncdir) = init_repo();
+
+    let repos = vec![reponame.clone()];
+    let testpkg = Package::new("testpkg1", "testpkg1", "1.0-1", vec![], vec![]);
+    let pkgnames = vec![testpkg.name.clone()];
+
+    let header = testpkg.tarheader();
+    let desc = testpkg.desc();
     let data = desc.as_bytes();
-    let datalen = u64::try_from(desc.len()).unwrap();
-    header.set_path("testpkg1-1.0-1/desc").unwrap();
-    header.set_size(datalen);
-    header.set_mode(0o644);
-    header.set_cksum();
 
     let mut archive = TarBuilder::new(Vec::new());
     archive.append(&header, data).unwrap();
     archive.finish().unwrap();
     let data = archive.into_inner().unwrap();
 
-    let mut afile = File::create(format!("{}/{}", syncdir, "test.db")).unwrap();
+    let mut afile = File::create(format!("{}/{}.db", syncdir, reponame)).unwrap();
+    afile.write_all(&data).unwrap();
+
+    (pkgnames, Some(dbpath), repos, rootdir)
+}
+
+#[fixture]
+fn reverse_deps() -> (Vec<String>, Option<String>, Vec<String>, TempDir) {
+    let reponame = String::from("test");
+    let (rootdir, dbpath, syncdir) = init_repo();
+
+    let repos = vec![reponame.clone()];
+    let testpkg = Package::new("testpkg1", "testpkg1", "1.0-1", vec![], vec![]);
+    let testpkg2 = Package::new(
+        "testpkg2",
+        "testpkg2",
+        "1.0-1",
+        vec!["testpkg1".to_string()],
+        vec![],
+    );
+    let pkgnames = vec![testpkg.name.clone(), testpkg2.name.clone()];
+
+    let mut archive = TarBuilder::new(Vec::new());
+
+    let header = testpkg.tarheader();
+    let desc = testpkg.desc();
+    let data = desc.as_bytes();
+    archive.append(&header, data).unwrap();
+
+    let header2 = testpkg2.tarheader();
+    let desc2 = testpkg2.desc();
+    let data2 = desc2.as_bytes();
+    archive.append(&header2, data2).unwrap();
+
+    archive.finish().unwrap();
+    let data = archive.into_inner().unwrap();
+
+    let mut afile = File::create(format!("{}/{}.db", syncdir, reponame)).unwrap();
     afile.write_all(&data).unwrap();
 
     (pkgnames, Some(dbpath), repos, rootdir)
 }
 
 #[rstest]
-fn test_reverse_deps(no_reverse_deps: (Vec<String>, Option<String>, Vec<String>, TempDir)) {
+fn test_no_reverse_deps(no_reverse_deps: (Vec<String>, Option<String>, Vec<String>, TempDir)) {
     let pkgnames = no_reverse_deps.0;
     let dbpath = no_reverse_deps.1;
     let repos = no_reverse_deps.2;
 
     let res = rebuilder::run(pkgnames.clone(), dbpath, repos, None).unwrap();
     assert_eq!(pkgnames[0], res.trim());
+}
+
+#[rstest]
+fn test_reverse_deps(reverse_deps: (Vec<String>, Option<String>, Vec<String>, TempDir)) {
+    let pkgnames = reverse_deps.0.clone();
+    let pkgname = &pkgnames[0];
+    let dbpath = reverse_deps.1;
+    let repos = reverse_deps.2;
+
+    let res = rebuilder::run(vec![pkgname.to_string()], dbpath, repos, None).unwrap();
+    let res_pkgs: Vec<&str> = res.trim().split_ascii_whitespace().collect();
+    assert_eq!(pkgnames, res_pkgs);
 }
